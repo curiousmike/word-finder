583cfb855ba707d3589cb293ff5747ef32971a84
mcoustier <curiousmike@gmail.com>
2020-05-14 18:28:17 -0700

    First pass adding material-ui and some basic components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import Letter from "./components/letter.component";
+// import WordInput from "./components/wordInput.component";
+import WordList from "./components/wordList.component";
+// import Button from "./components/button.component";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
+  const [letterInput, setLetters] = useState("");
+
+  function handleSolveClick() {
+    console.log("final letters = ", letterInput);
+  }
+  function handleLetters(e) {
+    setLetters(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form noValidate autoComplete="off">
+        <TextField
+          id="filled-basic"
+          label="Enter letters"
+          onChange={handleLetters}
+        />
+      </form>
+      <Letter />
+      <WordList />
+      <Button variant="contained" color="primary" onClick={handleSolveClick}>
+        Solve
+      </Button>
     </div>
   );
 }
